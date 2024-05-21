89181d8a87c54582284acc339bd0e6ed37b3b7ba
Matthew Horridge <matthew.horridge@stanford.edu>
2024-05-21 10:16:27 -0700

    Use Java records for GitHubRepository

--- src/main/java/edu/stanford/protege/github/GitHubRepository.java
+++ src/main/java/edu/stanford/protege/github/GitHubRepository.java
@@ -3,49 +3,41 @@ package edu.stanford.protege.github;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.auto.value.AutoValue;
 
 import java.util.Objects;
 
 /**
  * A summary representation of a GitHub repository
  */
-@AutoValue
-@GwtCompatible(serializable = true)
-public abstract class GitHubRepository implements GitHubObject {
+public record GitHubRepository(@JsonProperty("id") long id,
+                               @JsonProperty("node_id") String nodeId,
+                               @JsonProperty("name") String name,
+                               @JsonProperty("full_name") String fullName,
+                               @JsonProperty("description") String description) implements GitHubObject {
 
-    @JsonCreator
-    public static GitHubRepository get(@JsonProperty("id") long id,
+    public GitHubRepository(@JsonProperty("id") long id,
                             @JsonProperty("node_id") String nodeId,
                             @JsonProperty("name") String name,
                             @JsonProperty("full_name") String fullName,
                             @JsonProperty("description") String description) {
-        return new AutoValue_GitHubRepository(id,
-                                              Objects.requireNonNullElse(nodeId, ""),
-                                              Objects.requireNonNullElse(name, ""),
-                                              Objects.requireNonNullElse(fullName, ""),
-                                              Objects.requireNonNullElse(description, ""));
+        this.id = id;
+        this.nodeId = Objects.requireNonNullElse(nodeId, "");
+        this.name = Objects.requireNonNullElse(name, "");
+        this.fullName = Objects.requireNonNullElse(fullName, "");
+        this.description = Objects.requireNonNullElse(description, "");
+    }
+
+    @JsonCreator
+    public static GitHubRepository get(@JsonProperty("id") long id,
+                                       @JsonProperty("node_id") String nodeId,
+                                       @JsonProperty("name") String name,
+                                       @JsonProperty("full_name") String fullName,
+                                       @JsonProperty("description") String description) {
+        return new GitHubRepository(id, nodeId, name, fullName, description);
     }
 
     @JsonIgnore
     public GitHubRepositoryCoordinates getCoordinates() {
         return GitHubRepositoryCoordinates.fromFullName(fullName());
     }
-
-    @Override
-    @JsonProperty("id")
-    public abstract long id();
-
-    @Override
-    @JsonProperty("node_id")
-    public abstract String nodeId();
-
-    @JsonProperty("name")
-    public abstract String name();
-
-    @JsonProperty("full_name")
-    public abstract String fullName();
-
-    @JsonProperty("description")
-    public abstract String description();
 }
